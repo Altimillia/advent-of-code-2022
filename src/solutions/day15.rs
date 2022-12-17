08fa665b91d98eee95f7ebcf912988c3f9a7fac8
unknown <deweesem0225@gmail.com>
2022-12-17 11:39:52 -0600

    Day 15 complete

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -1,11 +1,12 @@
-use std::{fmt::Display, ops::Range};
+use core::panic;
+use std::cmp;
+use num::{bigint, BigInt};
 
 use itertools::Itertools;
 use nom::{
     bytes::complete::tag,
-    character::complete::{char, digit1, line_ending},
+    character::complete::{digit1},
     combinator::{map, map_res, recognize, opt},
-    multi::separated_list1,
     sequence::{separated_pair, tuple, preceded},
     IResult,
 };
@@ -18,6 +19,31 @@ pub fn part_one(input: String) -> i32 {
     value
 }
 
+pub fn part_two(input: String) -> BigInt {
+    let sensors = get_sensors(input);
+    let point = find_the_beacon(sensors, 4000000);
+
+    calculate_frequency(point)
+}
+
+fn calculate_frequency(point: Point) -> BigInt {
+    let big_x = bigint::BigInt::from(point.x);
+    let frequency = big_x * bigint::BigInt::from(4000000) + bigint::BigInt::from(point.y);
+    return frequency;
+}
+
+fn find_the_beacon(sensors: Vec<Sensor>, max_coord: i32) -> Point {
+    for row in 0..=max_coord {
+        let occupied_spaces = get_occupied_spaces_in_row_with_range(sensors.to_vec(), row, 0, max_coord);
+
+        if occupied_spaces != max_coord {
+            let x_location = find_x_location_of_beacon(sensors.to_vec(), row, 0, max_coord);
+            return Point::new(x_location, row);
+        }
+    }
+    panic!("Didn't find it");
+}
+
 fn get_sensors(input: String) -> Vec<Sensor> {
     input
     .lines()
@@ -25,20 +51,82 @@ fn get_sensors(input: String) -> Vec<Sensor> {
     .collect_vec()
 }
 
+fn get_occupied_spaces_in_row_with_range(sensors: Vec<Sensor>, row: i32, min: i32, max: i32) -> i32 {
+    let unique_values = flatten_ranges(&sensors
+        .iter()
+        .map(|sensor| sensor.get_covered_range_in_row_with_range(row, min, max))
+        .filter(|pair| pair.is_some())
+        .map(|pair| pair.unwrap())
+        .collect::<std::collections::BinaryHeap<_>>().into_sorted_vec());
+
+        unique_values.iter().map(|(s, e)| 
+        {
+            e - s}
+        ).sum()
+}
+
+fn find_x_location_of_beacon(sensors: Vec<Sensor>, row: i32, min: i32, max: i32) -> i32 { 
+    let unique_values = flatten_ranges(&sensors
+        .iter()
+        .map(|sensor| sensor.get_covered_range_in_row_with_range(row, min, max))
+        .filter(|pair| pair.is_some())
+        .map(|pair| pair.unwrap())
+        .collect::<std::collections::BinaryHeap<_>>().into_sorted_vec());
+
+
+    if unique_values.len() == 2 { 
+        let first = unique_values.get(0).unwrap();
+        return first.1 + 1;
+    }
+
+    if unique_values.len() == 1 {
+        let line = unique_values.get(0).unwrap();
+
+        return match line.0.cmp(&min) {
+            cmp::Ordering::Greater => min,
+            _ => max
+        }
+    }
+
+    panic!("Help");
+}
+
 fn get_occupied_spaces_in_row(sensors: Vec<Sensor>, row: i32) -> i32 {
-    let unique_values = sensors
-    .iter()
-    .map(|sensor| sensor.get_covered_range_in_row(10))
-    .map(|range| range.0..range.1)
-    .flatten()
-    .unique().collect_vec();
 
-    unique_values.len() as i32
+    let unique_values = flatten_ranges(&sensors
+        .iter()
+        .map(|sensor| sensor.get_covered_range_in_row(row))
+        .filter(|pair| pair.is_some())
+        .map(|pair| pair.unwrap())
+        .collect::<std::collections::BinaryHeap<_>>().into_sorted_vec());
+
+
+    unique_values.iter().map(|(s, e)| 
+    {
+        e - s}
+    ).sum()
 }
 
-pub fn part_two(input: String) -> impl Display {
-    0
+// https://www.geeksforgeeks.org/merging-intervals/
+fn flatten_ranges(ranges: &[(i32, i32)]) -> Vec<(i32, i32)> {
+    if ranges.len() == 0 {
+        return Vec::new()
+    }
+    (!ranges.is_empty()).then_some(ranges.iter()
+        .skip(1)
+        .fold(vec![ranges[0]], |flat, &r|
+            std::iter::once(flat.len() - 1).fold(flat, |mut flat, last| {
+                match flat[last].0 <= r.0 && r.0 <= flat[last].1 {
+                    true => flat[last].1 = flat[last].1.max(r.1),
+                    _ => flat.push(r),
+                }
+
+                flat
+            })))
+        .unwrap_or_default()
 }
+
+
 fn parse_numbers(input: &str) -> IResult<&str, i32> {
     let (i, number) = map_res(recognize(preceded(opt(tag("-")), digit1)), |s| {
         i32::from_str(s)
@@ -46,16 +134,23 @@ fn parse_numbers(input: &str) -> IResult<&str, i32> {
 
     Ok((i, number))
 }
+
+fn manhattan_distance(p1:Point, p2:Point) -> i32 {
+    (p1.x - p2.x).abs() + (p1.y - p2.y).abs()
+}
+
+#[derive(Clone, Copy)]
 struct Sensor {
     closest_beacon: Point,
-    position: Point
+    position: Point,
+    distance: i32
 }
 
 impl Sensor {
     fn parse(input: &str) -> IResult<&str, Self> {
         let (input, _) = tag("Sensor at ")(input)?;
         let parse_two_points = separated_pair(Sensor::parse_sensor, tag(": closest beacon is at "), Sensor::parse_sensor);
-        map(parse_two_points, |(p0, p1)| Sensor { position: p0, closest_beacon: p1})(input)
+        map(parse_two_points, |(p0, p1)| Sensor { position: p0, closest_beacon: p1, distance: manhattan_distance(p0, p1)})(input)
     }
 
     fn parse_sensor(input: &str) -> IResult<&str, Point> {
@@ -65,43 +160,25 @@ impl Sensor {
         Ok((input, Point::new(num1, num2)))
     }
 
-    fn get_covered_positions(&self) -> Vec<Point> {
+    fn get_covered_range_in_row(&self, y_row: i32) -> Option<(i32, i32)> {
 
-
-        let mut rt_value:Vec<Point> = Vec::new();
-        let mut check_now:Vec<Point> = Vec::new();
-
-        check_now.push(self.position);
-
-        rt_value.push(self.position);
-
-        while check_now.len() > 0 || !rt_value.contains(&self.closest_beacon) {
-            let p = check_now.remove(0);
-
-            for next in p.get_neighbors() {
-                check_now.push(next);
-                rt_value.push(next);
-                println!("{}",next);
-            }
-        }
-
-        rt_value
-
-    }
-
-    fn get_manhattan_distance(&self) -> i32 {
-        return (self.closest_beacon.x - self.position.x).abs() + (self.closest_beacon.y - self.position.y).abs()
-    }
-
-    fn get_covered_range_in_row(&self, y_row: i32) -> (i32, i32) {
-        let manhattan_distance = self.get_manhattan_distance();
-        let x_radia = manhattan_distance - (self.position.y - y_row).abs();
+        let x_radia = self.distance - (self.position.y - y_row).abs();
         if x_radia <= 0 {
-            return (0,0);
+            return Option::None
         }
-        println!("{}", x_radia);
 
-        return (self.position.x - x_radia, self.position.x + x_radia);
+        return Option::Some((self.position.x - x_radia, self.position.x + x_radia));
+    }
+
+    
+    fn get_covered_range_in_row_with_range(&self, y_row: i32, min: i32, max: i32) -> Option<(i32, i32)> {
+
+        let x_radia = self.distance - (self.position.y - y_row).abs();
+        if x_radia <= 0 {
+            return Option::None
+        }
+
+        return Option::Some((cmp::max(self.position.x - x_radia, min), cmp::min(self.position.x + x_radia, max)));
     }
 }
 
@@ -134,4 +211,36 @@ mod tests {
 
         assert_eq!(result, 26);
     } 
+    #[test]
+    fn sensor_can_calculate_its_coverage_distance() {
+        let input = "Sensor at x=8, y=7: closest beacon is at x=2, y=10";
+
+        
+        let sensors = get_sensors(input.to_string());
+        let result = sensors.get(0).unwrap().distance;
+
+        assert_eq!(result, 9);
+    }
+
+    #[test]
+    fn sensor_can_get_coverage_on_single_line() {
+        let input = "Sensor at x=8, y=7: closest beacon is at x=2, y=10";
+
+        
+        let sensors = get_sensors(input.to_string());
+        let result = get_occupied_spaces_in_row(sensors, 7);
+        
+        assert_eq!(result, 18);
+    }
+
+    #[test]
+    fn sensor_can_get_no_coverage_for_far_line() {
+        let input = "Sensor at x=8, y=7: closest beacon is at x=2, y=10";
+
+        
+        let sensors = get_sensors(input.to_string());
+        let result = get_occupied_spaces_in_row(sensors, 2000);
+        
+        assert_eq!(result, 0);
+    }
 }
